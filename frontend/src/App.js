8b51960685ba53f83e47fadeaac5f0f5adc07c76
Aryan-aarya <aryanarya1142003@gmail.com>
2024-07-25 22:18:18 +0530

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
 import "./App.css";
-<<<<<<< HEAD
-import NavBar from "./component/NavBar";
-function App() {
-  return (
-    <div className="App w-screen h-screen flex flex-col">
-    <NavBar/>
-      <p>app</p>
-    </div>
-=======
 import Card from "./components/Card";
 import Topnav from "./components/Topnav";
+import NavBar from "./component/NavBar";
 
 function App() {
   return (    
     <div className="container">
       <Topnav />
+      <NavBar/>
       <Card />
     </div>    
->>>>>>> 9b0a208fb552d53d266b278fe0198f47230f1873
   );
 }
 
